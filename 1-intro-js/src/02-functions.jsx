03d5456cca7470733bd8fe1616d252139fffc3b6
Manuel Requena <48953482+GManuelRequena@users.noreply.github.com>
2023-07-15 18:11:39 -0300

    Update 02-functions.jsx

--- 1-intro-js/src/02-functions.jsx
+++ 1-intro-js/src/02-functions.jsx
@@ -1,14 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
-// import "./index.css";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
 // function sayHello(name = "Pepe", age = 0) {
 //   const greeting = `Hola Mundo function! ${name} edad ${age}`;
 
