c348f5e79a14561a05504d6d4c2365ace973e42d
Manuel Requena <48953482+GManuelRequena@users.noreply.github.com>
2023-07-16 18:23:06 -0300

    Create 16-DOM.jsx

---
+++ 1-intro-js/src/16-DOM.jsx
@@ -0,0 +1,19 @@
+const findAllUsers = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  const users = await response.json();
+  const ul = document.createElement("ul");
+
+  users.forEach((user) => {
+    const li = document.createElement("li");
+    li.innerText = user.name;
+    ul.append(li);
+    console.log(user.name);
+  });
+
+  document.getElementById("root").append(ul);
+};
+
+findAllUsers();
+
+console.log("Hola");
