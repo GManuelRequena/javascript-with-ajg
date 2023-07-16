11c3f307fd238ce63eedab90f17f53aae0888b18
Manuel Requena <48953482+GManuelRequena@users.noreply.github.com>
2023-07-16 18:23:05 -0300

    Create 15-async.jsx

---
+++ 1-intro-js/src/15-async.jsx
@@ -0,0 +1,13 @@
+// httpClient
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+const findAllUsers = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  return await response.json();
+};
+
+const users = await findAllUsers();
+console.log(users);
+console.log("Hola");
