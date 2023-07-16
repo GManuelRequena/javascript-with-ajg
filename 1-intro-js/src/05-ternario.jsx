dd0be73bf58716d84fd4f36a01c62800f540c114
Manuel Requena <48953482+GManuelRequena@users.noreply.github.com>
2023-07-16 15:00:06 -0300

    Create 05-ternario.jsx

---
+++ 1-intro-js/src/05-ternario.jsx
@@ -0,0 +1,22 @@
+const average = 5.5;
+let status2 = "";
+status2 = average >= 5.5 ? "Aprobado" : "Rechazado";
+
+// if (average >= 5.5) {
+//   status2 = "Aprobado";
+// } else {
+//   status2 = "Reprobado";
+// }
+
+console.log(`Resultado: " ${status2}`);
+
+let max = 0;
+
+const a = 5;
+const b = 8;
+const c = 12;
+
+max = a > b ? a : b;
+max = max > c ? max : c;
+
+console.log(`El numero mayor es: ${max}`);
