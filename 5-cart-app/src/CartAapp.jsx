826fe34dc0b015c22ac0b313dba12cf1bf5cb21e
Manuel Requena <48953482+GManuelRequena@users.noreply.github.com>
2023-08-29 12:29:32 -0300

    Navbar and Routes added

--- 5-cart-app/src/CartAapp.jsx
+++ 5-cart-app/src/CartAapp.jsx
@@ -1,25 +1,40 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import CartView from "./components/CartView";
 import CatalogueView from "./components/CatalogueView";
 import { useItemsCart } from "./hooks/useItemsCart";
+import { Navbar } from "./components/Navbar";
 
 const CartApp = () => {
   const { cartItems, handlerAddProductCart, handlerDeleteProductCart } =
     useItemsCart();
   return (
     <>
+      <Navbar />
       <div className="container my-4">
         <h3>CartApp</h3>
-        <CatalogueView handler={handlerAddProductCart} />
-        {cartItems?.length <= 0 || (
-          <div className="my-4 w-50">
-            <CartView
-              items={cartItems}
-              handlerDelete={(id) => handlerDeleteProductCart(id)}
-            />
-          </div>
-        )}
-        {/* <div className="my-4 w-50"></div>
-        <CartView items={cartItems} handler={handlerDeleteProductCart} /> */}
+        <Routes>
+          <Route
+            path="catalog"
+            element={<CatalogueView handler={handlerAddProductCart} />}
+          />
+          <Route
+            path="cart"
+            element={
+              cartItems?.length <= 0 ? (
+                <div className="alert alert-warning">No products in cart</div>
+              ) : (
+                <div className="my-4 w-50">
+                  <CartView
+                    items={cartItems}
+                    handlerDelete={handlerDeleteProductCart}
+                  />
+                </div>
+              )
+            }
+          />
+
+          <Route path="/" element={<Navigate to={"catalog"} />} />
+        </Routes>
       </div>
     </>
   );
