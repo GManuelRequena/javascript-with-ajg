433ba528ad94adad5591959c2780cfb9969ab830
Manuel Requena <48953482+GManuelRequena@users.noreply.github.com>
2023-09-02 10:20:59 -0300

    Something

--- 6-users-app/src/UsersApp.jsx
+++ 6-users-app/src/UsersApp.jsx
@@ -1,37 +1,10 @@
-import { useReducer } from "react";
 import { LoginPage } from "./auth/pages/LoginPage";
-import { loginReducer } from "./auth/reducers/loginReducer";
-import Swal from "sweetalert2";
 import { UsersPage } from "./pages/UsersPage";
 import { Navbar } from "./components/Navbar";
-
-const initialLoigin = JSON.parse(sessionStorage.getItem("login")) || {
-  isAuth: false,
-  user: undefined,
-};
+import { useAuth } from "./auth/hooks/useAuth";
 
 export const UsersApp = () => {
-  const [login, dispatch] = useReducer(loginReducer, initialLoigin);
-  const handlerLogin = ({ username, password }) => {
-    if (username === "admin" && password === "12345") {
-      const user = { username: "admin" };
-      dispatch({
-        type: "login",
-        payload: user,
-      });
-      sessionStorage.setItem("login", JSON.stringify({ isAuth: true, user }));
-    } else {
-      Swal.fire("Validation Error", "Username or Password are wrong", "error");
-    }
-  };
-
-  const handlerLogOut = () => {
-    dispatch({
-      type: "logout",
-    });
-    sessionStorage.removeItem("login");
-  };
-
+  const { login, handlerLogin, handlerLogOut } = useAuth();
   return (
     <>
       {login.isAuth ? (
