4a2429ca961de6101b2e207b9ce7c4f22a40630b
Manuel Requena <48953482+GManuelRequena@users.noreply.github.com>
2023-09-16 00:42:15 -0300

    Update cart-app to use backend

--- 5-cart-app/src/components/CatalogueView.jsx
+++ 5-cart-app/src/components/CatalogueView.jsx
@@ -5,12 +5,21 @@ import PropTypes from "prop-types";
 
 const CatalogueView = ({ handler }) => {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
+  const getAllProds = async () => {
+    const prods = await getProducts();
+    setProducts(prods);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+  };
   useEffect(() => {
-    setProducts(getProducts());
+    getAllProds();
   }, []);
   return (
     <>
+      {isLoading && <div className="alert alert-info">Loading...</div>}
       <div className="row">
         {products.map((prod) => (
           <div className="col-4 my-2" key={prod.id}>
--- 5-cart-app/src/services/productService.js
+++ 5-cart-app/src/services/productService.js
@@ -1,6 +1,6 @@
-import products from "../data/products";
-
-const getProducts = () => {
+const getProducts = async () => {
+  const response = await fetch("http://localhost:8080/api/v1/products/all");
+  const products = await response.json();
   return products;
 };
 
